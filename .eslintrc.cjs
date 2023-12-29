65d1fcc51246ee1064ef53a538284faba5f1f02e
Vlad Chepurnyi <chepurnojv@gmail.com>
2023-12-29 11:55:03 +0200

    splittin template into components, changing vue foramtting rules, modal store

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,7 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
   overrides: [
     {
       files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/support/**/*.{js,ts,jsx,tsx}'],
@@ -19,5 +15,8 @@ module.exports = {
   },
   env: {
     node: true
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off'
   }
 }
