ef0cb5dcf360d4b213337446ff567176994620c9
Vlad Chepurnyi <chepurnojv@gmail.com>
2024-01-18 11:37:07 +0200

    navigating with links, custom links, tailwing styles, naming routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,21 +2,32 @@ import { createRouter, createWebHistory } from 'vue-router'
 // createWebHashHistory
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
+import Manage from '@/views/Manage.vue'
 
 const routes = [
   {
+    name: 'home',
     path: '/',
     component: Home
   },
   {
+    name: 'about',
+
     path: '/about',
     component: About
+  },
+  {
+    name: 'manage',
+
+    path: '/manage',
+    component: Manage
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
 })
 
 export default router
