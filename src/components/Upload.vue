fe697f7fb840b330ac0e2c03740e7a40add274bc
Vlad Chepurnyi <chepurnojv@gmail.com>
2024-01-26 13:12:08 +0200

    dynamic progress bar, handling success/error uploads

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -23,12 +23,15 @@
       <!-- Progess Bars -->
       <div class="mb-4" v-for="upload in uploads" :key="upload.name">
         <!-- File Name -->
-        <div class="font-bold text-sm">{{ upload.name }}</div>
+        <div class="font-bold text-sm" :class="upload.text_class">
+          <i :class="upload.icon"></i>
+          {{ upload.name }}
+        </div>
         <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
           <!-- Inner Progress Bar -->
           <div
-            class="transition-all progress-bar bg-blue-400"
-            :class="'bg-blue-400'"
+            class="transition-all progress-bar"
+            :class="upload.variant"
             :style="{ width: upload.current_progress + '%' }"
           ></div>
         </div>
@@ -62,15 +65,34 @@ export default {
         const songsRef = storageRef.child(`songs/${file.name}`) // music-4c52e.appspot.com/songs/example.mp3
         const task = songsRef.put(file)
 
-        this.uploads.push({
-          task,
-          current_progress: 0,
-          name: file.name
-        })
+        const uploadIndex =
+          this.uploads.push({
+            task,
+            current_progress: 0,
+            name: file.name,
+            variant: 'bg-blue-400',
+            icon: 'fas fa-spinner fa-spin',
+            text_class: ''
+          }) - 1
 
-        task.on('state_changed', (snapshot) => {
-          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        })
+        task.on(
+          'state_changed',
+          (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            this.uploads[uploadIndex].current_progress = progress
+          },
+          (error) => {
+            this.uploads[uploadIndex].variant = 'bg-red-400'
+            this.uploads[uploadIndex].icon = 'fas fa-times'
+            this.uploads[uploadIndex].text_class = 'text-red-400'
+            console.log(error)
+          },
+          () => {
+            this.uploads[uploadIndex].variant = 'bg-gree-400'
+            this.uploads[uploadIndex].icon = 'fas fa-check'
+            this.uploads[uploadIndex].text_class = 'text-green-400'
+          }
+        )
       })
 
       console.log(files)
