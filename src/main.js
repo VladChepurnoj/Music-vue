0723d1bf7ecd27ba1ef1415b7ee3d0c50724e773
Vlad Chepurnyi <chepurnojv@gmail.com>
2024-02-22 14:31:29 +0200

    the first translation, formatting and pluralization

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import './assets/tailwind.css'
 import './assets/main.css'
 import { auth } from './includes/firebase'
 import Icon from './directives/icon'
+import i18n from './includes/i18n'
 
 let app
 
@@ -20,6 +21,7 @@ auth.onAuthStateChanged(() => {
     app.use(createPinia())
     app.use(router)
     app.use(VeeValidatePlugin)
+    app.use(i18n)
     app.directive('icon', Icon)
 
     app.mount('#app')
